c3f0883c5a10617dbd6d730bed42c0c8d75dbd1b
Niloufar Alipour Tehrani <niloo.alt@gmail.com>
2025-03-02 14:38:27 +0100

    An example of fine-tuning the model from HuggingFace

---
+++ 2_FineTuneTransformer.py
@@ -0,0 +1,90 @@
+#%%
+from datasets import load_dataset
+import pandas as pd
+import matplotlib.pyplot as plt
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+import torch
+
+from transformers import Trainer, TrainingArguments
+
+
+from sklearn.metrics import accuracy_score, f1_score
+import numpy as np
+
+#%% Load the dataset
+emotions = load_dataset("emotion")
+
+
+
+#%%
+num_labels = len(emotions["train"].unique("label"))
+model_ckpt = "distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+device = torch.device("mps")
+
+def tokenize(batch):
+    return tokenizer(batch["text"], padding = True, truncation = True)
+
+emotions_encoded = emotions.map(tokenize, batched = True, batch_size=None)
+
+#%% Load pre-trained model
+model = (AutoModelForSequenceClassification
+         .from_pretrained(model_ckpt, num_labels=num_labels)
+         .to(device))
+
+# Warning because classification head has not yet been trained
+
+# %% Performance metrics
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    acc = accuracy_score(labels, preds)
+    f1 = f1_score(labels, preds, average="weighted")
+    return {"accuracy": acc, "f1": f1}
+# %%
+batch_size = 64
+logging_steps = len(emotions_encoded["train"]) // batch_size
+model_name = f"{model_ckpt}-finetuned-emotion"
+training_args = TrainingArguments(output_dir="./results",
+                                  num_train_epochs=2,
+                                  learning_rate=2e-5,
+                                  per_device_train_batch_size=batch_size,
+                                  per_device_eval_batch_size=batch_size,
+                                  weight_decay=0.01,
+                                  evaluation_strategy="epoch",
+                                  disable_tqdm=False,
+                                  logging_steps=logging_steps,
+                                  push_to_hub=False, # Don't push to huggingface
+                                  log_level="error")
+
+
+
+trainer = Trainer(model=model, args=training_args, 
+                  compute_metrics=compute_metrics,
+                  train_dataset=emotions_encoded["train"],
+                  eval_dataset=emotions_encoded["validation"],
+                  tokenizer=tokenizer)
+trainer.train();
+# %%
+preds_output = trainer.predict(emotions_encoded["validation"])
+preds_output.metrics
+# %% Confusion matrix
+
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+def plot_confusion_matrix(y_preds, y_true, labels):
+    cm = confusion_matrix(y_true, y_preds, normalize="true")
+    fig, ax = plt.subplots(figsize=(6, 6))
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+    disp.plot(cmap="Blues", values_format=".2f", ax=ax, colorbar=False)
+    plt.title("Normalized confusion matrix")
+    plt.show()
+    
+
+y_preds = np.argmax(preds_output.predictions, axis=1)
+plot_confusion_matrix(y_preds, y_valid, labels)
+# %% Save the model
+
+model.save_pretrained("./fine_tuned_bert")
+tokenizer.save_pretrained("./fine_tuned_bert")
